ca46f5e850e8ddd9ec5fcf097ae357b2983bf40b
Joaovitorufu <joaovitorufu@gmail.com>
2023-10-24 17:57:41 -0300

    desafio resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+            Nokia nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+            Console.WriteLine("\n");
+            Iphone iPhone = new Iphone("4987", "Modelo 2", "222222222", 128);
+            iPhone.Ligar();
+            iPhone.ReceberLigacao();
+            iPhone.InstalarAplicativo("Telegram");
+
+
+        
+    }
+}
